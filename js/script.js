e164f9f5764b3bda972e241dc727c6adfd86e042
Seu Nome <rickhard@bol.com.br>
2024-05-07 03:01:57 -0300

    carrocel de videos

--- js/script.js
+++ js/script.js
@@ -12,3 +12,42 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   });
 });
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true,
+        // Aqui estamos definindo a configuração appendArrows
+        appendArrows: $('.carousel-arrows'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
